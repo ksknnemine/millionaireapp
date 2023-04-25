fe9200eb35ce75d58f5dae93ba37e54861c6ab33
emine keskin <eminekeskin_09@hotmail.com>
2023-04-25 22:05:55 +0300

    fix: Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useMemo, useState } from 'react';
 import Trivia from './components/Trivia';
 import Timer from './components/Timer';
+import Start from './components/Start';
 import './app.css';
 
 function App() {
+  const [username, setUsername] = useState(null);
   const [questionNumber, setQuestionNumber] = useState(1);
   const [stop, setStop] = useState(false);
   const [earned, setEarned] = useState("$ 0");
@@ -105,38 +107,42 @@ function App() {
 
   return (
     <div className="app">
-      <div className="main">
-        {stop ? <h1 className="endText">You earned: {earned} </h1> : (
-          <>
-            <div className="top">
-              <div className="timer">
-                <Timer setStop={setStop} questionNumber={questionNumber} />
-              </div>
-            </div>
-            <div className="bottom">
-              <Trivia
-                data={data}
-                setStop={setStop}
-                questionNumber={questionNumber}
-                setQuestionNumber={setQuestionNumber}
-              />
-            </div>
-          </>
-        )}
+      {username ? (
+        <>
+          <div className="main">
+            {stop ? <h1 className="endText">You earned: {earned} </h1> : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer setStop={setStop} questionNumber={questionNumber} />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Trivia
+                    data={data}
+                    setStop={setStop}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                  />
+                </div>
+              </>
+            )}
 
-      </div>
-      <div className="pyramid">
-        <ul className="moneyList">
-          {moneyPyramid.map((m) => (
-            <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
-              <span className="moneyListItemNumber">{m.id}</span>
-              <span className="moneyListItemAmount">{m.amount}</span>
-            </li>
-          ))
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((m) => (
+                <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
+                  <span className="moneyListItemNumber">{m.id}</span>
+                  <span className="moneyListItemAmount">{m.amount}</span>
+                </li>
+              ))
 
-          }
-        </ul>
-      </div>
+              }
+            </ul>
+          </div>
+        </>
+      ) : <Start setUsername={setUsername} />}
     </div>
   );
 }
--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,9 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+import useSound from 'use-sound';
+import play from '../assets/play.mp3'
+import correct from '../assets/correct.mp3'
+import wrong from '../assets/wrong.mp3'
 
 export default function Trivia({ data, setStop, questionNumber, setQuestionNumber }) {
     const [question, setQuestion] = useState(null);
     const [selectedAnswer, setSelectedAnswer] = useState(null);
     const [className, setClassName] = useState("answer");
+    const [letsPlay] = useSound(play);
+    const [correctAnswer] = useSound(correct);
+    const [wrongAnswer] = useSound(wrong);
+
+    useEffect(() => {
+        letsPlay();
+    }, [letsPlay]);
 
     useEffect(() => {
         setQuestion(data[questionNumber - 1]);
@@ -21,14 +32,20 @@ export default function Trivia({ data, setStop, questionNumber, setQuestionNumbe
         delay(3000, () =>
             setClassName(a.correct ? "answer correct" : "answer wrong")
         );
-        delay(6000, ()=> {
-            if(a.correct){
-                setQuestionNumber((prev) => prev + 1);
-                setSelectedAnswer(null);
-            }else{
-                setStop(true);
+        delay(5000, () => {
+            if (a.correct) {
+                correctAnswer();
+                delay(1000, () => {
+                    setQuestionNumber((prev) => prev + 1);
+                    setSelectedAnswer(null);
+                });
+            } else {
+                wrongAnswer();
+                delay(1000, () => {
+                    setStop(true);
+                });
             }
-        })
+        });
     }
 
 
